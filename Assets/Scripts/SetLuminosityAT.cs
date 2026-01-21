15ed77735f13967b14c8bd889e735a48a2c39601
deramo <deramo@OA-S141-24>
2026-01-21 17:04:23 -0500

    more done

--- Assets/Scripts/SetLuminosityAT.cs
+++ Assets/Scripts/SetLuminosityAT.cs
@@ -6,8 +6,8 @@ namespace NodeCanvas.Tasks.Actions {
 
 	public class SetLuminosityAT : ActionTask {
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
+		public Light lightHouseLight;
+		public float luminosity;
 		protected override string OnInit() {
 			return null;
 		}
@@ -16,6 +16,7 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
+			lightHouseLight.intensity = luminosity;
 			EndAction(true);
 		}
 
