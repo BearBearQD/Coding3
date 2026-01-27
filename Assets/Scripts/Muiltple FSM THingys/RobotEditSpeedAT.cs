7fc3b4f0524319d1e9200befa9e6d817f14cc968
BearBearQD <86070469+BearBearQD@users.noreply.github.com>
2026-01-26 20:02:38 -0500

    Didnt Fully Finish but my time is up so im basscially done

--- Assets/Scripts/Muiltple FSM THingys/RobotEditSpeedAT.cs
+++ Assets/Scripts/Muiltple FSM THingys/RobotEditSpeedAT.cs
@@ -2,13 +2,11 @@ using NodeCanvas.Framework;
 using ParadoxNotion.Design;
 using UnityEngine;
 
-
 namespace NodeCanvas.Tasks.Actions {
 
-	public class RobotScanningAT : ActionTask {
-        public LayerMask targetMask;
-        public BBParameter<float> localCount;
-        public float scanRadius = 3f;
+	public class RobotEditSpeedAT : ActionTask {
+        public BBParameter<float> speed;
+		public BBParameter<float> localCount;
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
         protected override string OnInit() {
@@ -19,29 +17,18 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-            Collider[] objectsInRange = Physics.OverlapSphere(agent.transform.position, scanRadius, targetMask);
-
-            foreach (Collider objectInRange in objectsInRange)
-            {
-                Blackboard pillarBlackBoard = objectInRange.GetComponentInParent<Blackboard>();
-                float pillarEncounterCount = pillarBlackBoard.GetVariableValue<float>("count");
-                pillarEncounterCount++;
-                pillarBlackBoard.SetVariableValue("count", pillarEncounterCount);
-                localCount.value++;
-                Debug.Log("Clap");
-            }
-            EndAction(true);
-        }
+			speed.value = localCount.value;
+		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-
-        }
+			
+		}
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
-
-        }
+			
+		}
 
 		//Called when the task is paused.
 		protected override void OnPause() {
